d7162ca243455f3dc4906626b869c4f96a541a4a
Pedro Garcias <pedro.garcias@gmail.com>
2020-05-20 20:45:40 -0300

    table ready

--- src/commentary_game/data_preparation.py
+++ src/commentary_game/data_preparation.py
@@ -14,14 +14,14 @@ def get_links():
     # make a driver to create a section were we going to work, and get the source of page
     driver = webdriver.Chrome(executable_path=Params.path_crome)
     driver.get('https://www.foxsports.com./soccer/schedule?competition=4&season=2019&round=1&week=0&group=0&sequence=1')
-    WebDriverWait(driver, 10).until(
+    WebDriverWait(driver, 60).until(
         EC.presence_of_element_located((By.XPATH, '//*[@id="wisfoxbox"]/section[3]/div[1]/div/div[1]/div[2]/a')))
     html = driver.page_source
     # read the page source with BeautifulSoup, and make a lista with all links game
     soup = BeautifulSoup(html, 'lxml')
     urls = soup.find_all('td', {'class': 'wisbb_gameInfo'})
     # with list comprehension select only the games have ended
-    links = ['https://www.foxsports.com.' + item.find('a')['href'] for item in urls if
+    links = ['https://www.foxsports.com' + item.find('a')['href'] for item in urls if
              item.find('span', {'class': 'wisbb_status'}).text == 'FINAL']
     # end a driver and return a list with links
     driver.quit()
--- streamlit/app.py
+++ streamlit/app.py
@@ -1,14 +1,17 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from league_stats import ger_table
-from get_inputs import find_games_today
+from league_stats import ger_table, ger_final_results, first_final_round
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+#from get_inputs import find_games_today
 
 df=pd.read_csv('C:/Users/pedro/Projetos/bundesbet/src/data/game_stats.csv')
 
 def capa():
-    st.title('Bundesliga')
-    st.header(find_games_today())
+    pass
 
 def tabela():
     list_select = np.arange(df['round'].max(),0,-1)
@@ -24,8 +27,17 @@ def main():
     if add_selectbox == 'Home':
         capa()
 
-    elif add_selectbox == 'Tabela':
+    add_selectbox2 = st.sidebar.selectbox(
+        'Estatísticas',
+        ('Tabela','Resumo Campeonato','Teste'))
+
+    if add_selectbox2 == 'Tabela':
         tabela()
+    elif add_selectbox2 == 'Resumo Campeonato':
+        st.title('Estatísticas do campeonato')
+        st.plotly_chart(ger_final_results(df))
+        st.plotly_chart(first_final_round(df))
+
 
 
 
